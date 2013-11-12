2ddea1eed420fb7e3c16c5c7db381656c01a0da7
Barrett Avery <barrettavery@Barrett-Averys-MacBook-Pro.local>
2013-11-12 15:02:33 -0800

    uxbar.php commit

--- uxbar.php
+++ uxbar.php
@@ -1,1 +1,47 @@
-Test content
+<?php
+// this file will be used to dynamically build the side (or top)
+// nav menu on the map page.
+
+
+
+
+
+
+?>
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Bootstrap 101 Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="stylesheet.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/verse.js"></script>
+        <!--<script src="<<filename>>.js"></script>-->
+
+        <div>
+        	<img src="images/united-way-logo-aws.png">
+        </div>
+        Please enter your zipcode:
+        <div class="row"> 
+        	<div class="span2">
+			    <div class="input-group">
+			      <input type="text" class="form-control">
+			      <span class="input-group-btn">
+			        <button class="btn btn-default" id="zipcode" type="button">Go!</button>
+			      </span>
+			    </div><!-- /input-group -->
+			</div>
+		</div><!-- /.row -->
+
+
+
+
+    </body>
+    </html>
+
