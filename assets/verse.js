0b69e27b43fd0581c5cbaaac495193992efb9960
Derek Rose <bishma@gmail.com>
2013-11-12 18:21:10 -0800

    user position incorperated

--- assets/verse.js
+++ assets/verse.js
@@ -7,20 +7,23 @@ var mapDiv = 'map-canvas';
 
 // This is the location of the user we will attempt to use to initialize the map. Let's store a reference to it here so
 // that it can be updated in several places.
-var userLocation = null;
+var centerPoint = null;
 
 // global var for the google geocoder and dynamic map sizing
 var totalHeight, totalWidth, widthMode, resizeTimeout;
 var geocoder, mapWidth, mapHeight, navWidth, navHeight;
 
 $(document).ready(function() {
+	// detect current browser width/height and set environment accordingly
 	setLayout();
 
+	// listen for window size change
 	$(window).resize(function() {
 		if (resizeTimeout) {
 			clearTimeout(resizeTimeout);
 		}
 
+		// use a timeout so we aren't resizing constantly
 		resizeTimeout = setTimeout(function() {
 			setLayout();
 			google.maps.event.trigger(map, "resize");
@@ -31,14 +34,41 @@ $(document).ready(function() {
 	// instance a geocoder to allow users to set a ma center point via zipcode
 	geocoder = new google.maps.Geocoder();
 
-	var startCenter = new google.maps.LatLng(34.052234,-118.243685);
-	var mapOptions = {
-		zoom: 8,
-		center: startCenter,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
+	// get a center location (either from the user or roughtly in the center of LA) then draw the map
+	$.when(userLocation()).then(function(point) {
+		var centerPoint = new google.maps.LatLng(point.latitude,point.longitude);
+		var mapOptions = {
+			zoom: 8,
+			center: centerPoint,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+	});
 
-	var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+	function userLocation() {
+		var deferred = new $.Deferred();
+		var point = {"latitude":34.052234,"longitude":-118.243685};
+		deferred.resolve(point);
+
+		// navigator.geolocation.getCurrentPosition(function(position){
+		// 	var point = {"latitude":position.coords.latitude,"longitude":position.coords.longitude};
+		// 	deferred.resolve(point);
+		// },
+		// function(error) {
+		// 	if (console)
+		// 		console.log('Unable to get user position: '+error);
+
+		// 	var point = {"latitude":34.052234,"longitude":-118.243685};
+		// 	deferred.resolve(point);
+		// },
+		// {
+		// 	timeout: 20000,
+		// 	enableHighAccuracy: true
+		// });
+
+		// return promise so that outside code cannot reject/resolve the deferred
+		return deferred.promise();
+	};
 });
 
 function setLayout() {
@@ -66,35 +96,3 @@ function setLayout() {
 	$('#'+navDiv).width(navWidth).height(navHeight);
 	$('#'+mapDiv).width(mapWidth).height(mapHeight);
 }
-
-/*if (navigator.geolocation) {
-	// Get the location of the user's browser using the native geolocation service. When we invoke this method
-	// only the first callback is requied. The second callback - the error handler - and the third
-	// argument - our configuration options - are optional.
-	navigator.geolocation.getCurrentPosition(
-		function(position) {
-		 
-			// Check to see if there is already a location. There is a bug in FireFox where this gets
-			// invoked more than once with a cahced result.
-			if (userLocation) {
-				return userLocation;
-			}
- 
-			// Add a marker to the map using the position.
-			userLocation = {
-				"latitude" : position.coords.latitude,
-				"longitude" : position.coords.longitude
-			};
- 
-		},
-		function(error) {
-			if (console) {
-				console.log( "Something went wrong: ", error );
-			}
-		},
-		{
-			timeout: (5 * 1000),
-			enableHighAccuracy: true
-		}
-	);
-}*/
