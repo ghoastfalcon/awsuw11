03b3cbbf53e0dda2e800f21b8690bcd5d08a7dd2
Derek Rose <bishma@gmail.com>
2013-11-12 17:08:53 -0800

    Dynmamic layout switching via javascript

--- index.php
+++ index.php
@@ -17,60 +17,38 @@
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-  	<meta charset="UTF-8">
-    <title>United Way Team 11!</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="<?=$assetProto.$assetDomain?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<head>
+		<meta charset="UTF-8">
+		<title>United Way Team 11!</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<!-- Bootstrap -->
+		<link href="<?=$assetProto.$assetDomain?>/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+		<!-- Custom CSS -->
+		<link href="<?=$assetProto.$assetDomain?>/masterstyle.css" rel="stylesheet" />
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-    <style type="text/css">
-    	body, html {
-    		height: 100%;
-    		margin: 0px;
-    		padding: 0px;
-    	}
-      	.full {
-      		height: 100%
-      	}
-      	#ux-bar {
-      		background-color: #0F0;
-      	}
-      	#map-canvas {
-      		width: 100%;
-      		height: 100%;
-      	}
-    </style>
-  </head>
-  <body>
-  	<div class="row full">
-        <div class="col-md-2 full" id="ux-bar">
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+	 		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div id="ux-bar">
 <?php
 	include('uxbar.php');
 ?>
-        </div>
-        <div class="col-md-10 full">
-        	<div id="map-canvas"></div>
-        </div>
-    </div>
-    
+		</div>
+		<div id="map-canvas"></div>
+	
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="<?=$assetProto.$assetDomain?>/jquery-2.0.2%2Cjs"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="<?=$assetProto.$assetDomain?>/bootstrap/js/bootstrap.js"></script>
-    <!-- Google Maps -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAPNqNLrWRSdshfgppSteYx_8qmO6lqtk4&sensor=<?=$hasGPS?>" type="text/javascript"></script>
-    <script src="<?=$assetProto.$assetDomain?>/verse.js" type="text/javascript">
-    	
-
-		
-	</script>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="<?=$assetProto.$assetDomain?>/jquery-2.0.2%2Cjs"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="<?=$assetProto.$assetDomain?>/bootstrap/js/bootstrap.js"></script>
+		<!-- Google Maps -->
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAPNqNLrWRSdshfgppSteYx_8qmO6lqtk4&sensor=<?=$hasGPS?>" type="text/javascript"></script>
+		<!-- Custom JS -->
+		<script src="<?=$assetProto.$assetDomain?>/verse.js" type="text/javascript"></script>
   </body>
 </html>
